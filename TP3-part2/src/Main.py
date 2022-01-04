83aa28c0802b7ca7f54a9c80d7df7645ade59a6c
Brocowlee <93135192+Brocowlee@users.noreply.github.com>
2022-01-04 11:54:32 +0100

    Add files via upload

---
+++ TP3-part2/src/Main.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 19:47:50 2017
+
+@author: lfoul
+"""
+
+from Configuration import Configuration
+from Section import Section
+from Wall import Wall
+from Door import Door
+from Window import Window
+from House import House
+from Opening import Opening
+import copy
+import pygame
+import OpenGL.GL as gl
+import OpenGL.GLU as glu
+
+'''
+if __name__ == '__main__':
+    pygame.init()
+    display=(600,600)
+    pygame.display.set_mode(display, pygame.DOUBLEBUF | pygame.OPENGL)
+    
+    # Sets the screen color (white)
+    gl.glClearColor(1, 1, 1, 1)
+    # Clears the buffers and sets DEPTH_TEST to remove hidden surfaces
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)                  
+    gl.glEnable(gl.GL_DEPTH_TEST)    
+    
+    glu.gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    gl.glTranslatef(0.0, 2, -5)
+    gl.glRotatef(-90, 1, 0, 0)
+    gl.glBegin(gl.GL_LINES) # Indique que l'on va commencer un trace en mode lignes (segments)
+    gl.glColor3fv([0, 0, 0]) # Indique la couleur du prochian segment en RGB
+    gl.glVertex3fv((0,0, -2)) # Premier vertice : départ de la ligne
+    gl.glVertex3fv((1, 1, -2)) # Deuxième vertice : fin de la ligne
+    gl.glEnd() # Find du tracé
+    
+    pygame.display.flip() # Met à jour l'affichage de la fenêtre graphique
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+'''
+
+
+def Q1a():
+    return Configuration()
+  
+    
+def Q1b_f():
+    return Configuration({'screenPosition': -5, 'xAxisColor': [1, 1, 0]}). \
+        setParameter('xAxisColor', [1, 1, 0]). \
+        setParameter('yAxisColor', [0,1,1]). \
+        display()
+        
+def Q2b():
+    # Ecriture en utilisant le chaînage
+    return Configuration().add(
+            Section({'position': [1, 1, 0], 'width':7, 'height':2.6})
+            ) 
+
+def Q2c():
+    # Ecriture en utilisant le chaînage
+    return Configuration().add(
+            Section({'position': [1, 1, 0], 'width':7, 'height':2.6, 'edges': True})
+            )
+
+def Q3a():
+    return Configuration().add(
+            Wall({'position': [1, 1, 0], 'width':7, 'height':2.6, 'edges':True})
+            )  
+
+def Q4a():
+    # Ecriture en utilisant des variables : A compléter
+    wall1 = Wall({'position': [1, 1, 0], 'width':7, 'height':2.6, 'edges':True})
+    wall2 = Wall({'position': [1, 5, 0], 'width':7, 'height':2.6, 'edges':True})
+    wall3 = Wall({'position': [1, -1, 0], 'width':4.2, 'height':2.6, 'edges':True,'orientation':90})
+    wall4 = Wall({'position': [1, -8, 0], 'width':4.2, 'height':2.6, 'edges':True, 'orientation':90})  
+    house = House({'position': [-3, 1, 0], 'orientation':0})
+    house.add(wall1).add(wall3).add(wall4).add(wall2)
+    return Configuration().add(house)   
+    
+def Q5a():  
+    # Ecriture avec mélange de variable et de chaînage    
+    opening1 = Opening({'position': [2, 0, 0], 'width':0.9, 'height':2.15, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]})
+    opening2 = Opening({'position': [4, 0, 1.2], 'width':1.25, 'height':1, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]})    
+    return Configuration().add(opening1).add(opening2)
+    
+def Q5b():  
+    # Ecriture avec mélange de variable et de chaînage   
+    section = Section({'width':7, 'height':2.6})
+    opening1 = Opening({'position': [2, 0, 0], 'width':0.9, 'height':2.15, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]})
+    opening2 = Opening({'position': [4, 0, 1.2], 'width':1.25, 'height':1, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]}) 
+    opening3 = Opening({'position': [4, 0, 1.7], 'width':1.25, 'height':1, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]}) 
+    
+    print(section.canCreateOpening(opening1))
+    print(section.canCreateOpening(opening2))    
+    print(section.canCreateOpening(opening3))
+    return Configuration()    
+    
+def Q5c1():      
+    section = Section({'width':7, 'height':2.6})
+    opening1 = Opening({'position': [2, 0, 0], 'width':0.9, 'height':2.15, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]})
+    sections = section.createOpening(opening1)
+    configuration = Configuration()
+    for x in sections:
+        configuration.add(x)    
+    return configuration     
+    
+def Q5c2():      
+    section = Section({'width':7, 'height':2.6})
+    opening2 = Opening({'position': [4, 0, 1.2], 'width':1.25, 'height':1, 'thickness':0.2, 'color': [0.7, 0.7, 0.7]}) 
+    sections = section.createNewSections(opening2)
+    configuration = Configuration()
+    for section in sections:
+        configuration.add(section)    
+    return configuration    
+
+def Q5d():      
+    pass
+    
+def Q6():  
+    pass  
+ 
+def main():
+    # Enlever un des commentaires pour la question traitée
+    
+    #configuration = Q1a()
+    #configuration = Q1b_f()
+    #configuration = Q2b()
+    # configuration = Q2c()
+    #configuration = Q3a()
+    configuration = Q4a()
+    # configuration = Q5a()
+    # configuration = Q5b()
+    # configuration = Q5c1()
+    # configuration = Q5c2() 
+    # configuration = Q5d()
+    # configuration = Q6()
+    configuration.display()     
+         
+# Calls the main function
+if __name__ == "__main__":
+    main()    
---
+++ TP3-part2/src/Section.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 19:47:50 2017
+
+@author: lfoul
+"""
+import OpenGL.GL as gl
+
+class Section:
+    # Constructor
+    def __init__(self, parameters = {}) :  
+        # Parameters
+        # position: position of the wall 
+        # width: width of the wall - mandatory
+        # height: height of the wall - mandatory
+        # thickness: thickness of the wall
+        # color: color of the wall        
+
+        # Sets the parameters
+        self.parameters = parameters
+        
+        # Sets the default parameters
+        if 'position' not in self.parameters:
+            self.parameters['position'] = [0, 0, 0]        
+        if 'width' not in self.parameters:
+            raise Exception('Parameter "width" required.')   
+        if 'height' not in self.parameters:
+            raise Exception('Parameter "height" required.')   
+        if 'orientation' not in self.parameters:
+            self.parameters['orientation'] = 0              
+        if 'thickness' not in self.parameters:
+            self.parameters['thickness'] = 0.2    
+        if 'color' not in self.parameters:
+            self.parameters['color'] = [0.5, 0.5, 0.5]       
+        if 'edges' not in self.parameters:
+            self.parameters['edges'] = False             
+            
+        # Objects list
+        self.objects = []
+
+        # Generates the wall from parameters
+        self.generate()   
+        
+    # Getter
+    def getParameter(self, parameterKey):
+        return self.parameters[parameterKey]
+    
+    # Setter
+    def setParameter(self, parameterKey, parameterValue):
+        self.parameters[parameterKey] = parameterValue
+        return self     
+
+    # Defines the vertices and faces 
+    def generate(self):
+        self.vertices = [
+               [0, 0, 0 ],
+               [0, 0, self.parameters['height']],
+               [self.parameters['width'], 0, self.parameters['height']],
+               [self.parameters['width'], 0, 0],
+               [0, self.parameters['thickness'], 0 ],
+               [0, self.parameters['thickness'], self.parameters['height']],
+               [self.parameters['width'], self.parameters['thickness'], self.parameters['height']],
+               [self.parameters['width'], self.parameters['thickness'], 0]]
+              
+        self.faces = [
+               [0,1,2,3],
+               [0,1,5,4],
+               [0,4,7,3],
+               [2,6,3,7],
+               [0,7,7,3],
+               [4,5,6,7],
+               ] 
+   
+
+    # Checks if the opening can be created for the object x
+    def canCreateOpening(self, x):
+        # A compléter en remplaçant pass par votre code
+        pass      
+        
+    # Creates the new sections for the object x
+    def createNewSections(self, x):
+        # A compléter en remplaçant pass par votre code
+        pass              
+        
+    # Draws the edges
+    def drawEdges(self):
+        gl.glPushMatrix()
+        gl.glTranslatef(self.parameters['position'][0],self.parameters['position'][1],self.parameters['position'][2])
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE) # on trace les faces : GL_FILL
+        gl.glBegin(gl.GL_QUADS) # Tracé d’un quadrilatère
+        gl.glColor3fv([0, 0, 0]) # Couleur gris noir
+        for face in self.faces:
+          for vertex in face:
+            gl.glVertex3fv(self.vertices[vertex])
+        gl.glEnd()
+        gl.glPopMatrix()
+
+                    
+    # Draws the faces
+    def draw(self):
+        gl.glPushMatrix()
+        self.drawEdges()
+        gl.glTranslatef(self.parameters['position'][0],self.parameters['position'][1],self.parameters['position'][2])
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL) # on trace les faces : GL_FILL
+        gl.glBegin(gl.GL_QUADS) # Tracé d’un quadrilatère
+        gl.glColor3fv([0.5, 0.5, 0.5]) # Couleur gris moyen
+        for face in self.faces:
+          for vertex in face:
+            gl.glVertex3fv(self.vertices[vertex])
+        gl.glEnd()
+        gl.glPopMatrix()
+
+  
